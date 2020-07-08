f01bf5f040343f57fc1d9139c20f70615de75b1f
largecats <linfanxiaolinda@outlook.com>
2020-07-08 22:38:53 +0800

    increased patch number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pysqlformatter-largecats',  # Replace with your own username
-    version='0.0.10',
+    version='0.0.11',
     author='largecats',
     author_email='linfanxiaolinda@outlook.com',
     description='A formatter for Pyspark code with SQL queries.',
